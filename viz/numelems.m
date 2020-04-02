3f7b323bd406e929bbea007d9a7ff0a8af73e4ec
BrianOBlanton <Brian_Blanton@Renci.Org>
2020-04-01 21:01:39 -0400

    added node/elem numbering

--- viz/numelems.m
+++ viz/numelems.m
@@ -11,12 +11,7 @@ function h=numelems(fem_grid_struct,varargin)
 % Summer 1997
 %
 
-% DEFINE ERROR STRINGS 
-warn1=['The current axes is too dense for the  '
-       'node numbers to be readable.  CONTINUE?']; 
 
-ps=15;
-          
 % check arguments
 if nargin ==0 
    disp('h=numelems(fem_grid_struct,p1,v1,...);');
@@ -24,6 +19,19 @@ if nargin ==0
 end
 if ~is_valid_struct(fem_grid_struct)
    error('    Argument to NUMELEMS must be a valid fem_grid_struct.')
+end
+
+k=1;
+ps=15;
+ax=gca;
+while k<length(varargin)
+  switch lower(varargin{k})
+    case 'axes'
+      ax=varargin{k+1};
+      varargin([k k+1])=[];
+    otherwise
+      k=k+2;
+  end
 end
 
 
@@ -34,15 +42,14 @@ x=fem_grid_struct.x;
 y=fem_grid_struct.y;
 z=10;
 
-
 % compute centroid of each element 
 i=1:length(elems(:,1));
 xcent=(x(elems(i,1))+x(elems(i,2))+x(elems(i,3)))/3;
 ycent=(y(elems(i,1))+y(elems(i,2))+y(elems(i,3)))/3;
  
 % get indices of centroids within viewing window defined by X,Y
-X=get(gca,'XLim');
-Y=get(gca,'YLim');
+X=get(ax,'XLim');
+Y=get(ax,'YLim');
 
 % get indices of element centroids within viewing window defined by X,Y
 filt=find((xcent>X(1)&xcent<X(2))&(ycent>Y(1)&ycent<Y(2)));
@@ -73,7 +80,7 @@ yy=ycent(filt);
 zz=z*ones(size(xx));
 format long e
 % label only those nodes that lie within viewing window.
-htext=text(xx,yy,strlist,...
+htext=text(ax,xx,yy,strlist,...
                  'HorizontalAlignment','center',...
                  'VerticalAlignment','middle',...
                  'BackgroundColor','k',...
